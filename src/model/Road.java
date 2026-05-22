b9321f565751d5ca70b3135ce3efea8de2981eaf
Zeynepedaozturk <121388614+Zeynepedaozturk@users.noreply.github.com>
2026-05-22 14:26:56 +0300

    Add Road.java

---
+++ src/model/Road.java
@@ -0,0 +1,9 @@
+package model;
+
+// It enables the delivery of infrastructure resources to remote areas via BFS.
+public class Road extends Cell {
+
+    public Road(int x, int y) {
+        super(x, y, 'R');
+    }
+}
