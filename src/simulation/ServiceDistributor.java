d9a70862256b4987a31f37de9dfe963fe53d56ff
20220602049 <cagankaratas23@gmail.com>
2026-05-27 17:40:41 +0300

    Create ServiceDistributor.java
    "feat(task-2.1): ServiceDistributor class and constructor have added"

---
+++ src/simulation/ServiceDistributor.java
@@ -0,0 +1,18 @@
+package simulation;
+
+import model.CityGrid;
+import model.Cell;
+import model.ServiceBuilding;
+import model.Zone;
+import model.Hospital;
+import model.PoliceStation;
+import model.School;
+
+public class ServiceDistributor {
+
+    private CityGrid grid;
+
+    public ServiceDistributor(CityGrid grid) {
+        this.grid = grid;
+    }
+}
